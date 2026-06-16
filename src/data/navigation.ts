be63c0aaf5b723fe05842bb50332b72095076e42
Andy Araya Sanchez <andyarayacr@gmail.com>
2026-06-15 18:13:14 -0600

    Hide case studies section

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -2,7 +2,6 @@ export const navigation = [
   { label: 'Home', href: '#home' },
   { label: 'Credentials', href: '#credentials' },
   { label: 'Mindmap', href: '#mindmap' },
-  { label: 'Case Studies', href: '#case-studies' },
   { label: 'Strategic Reading', href: '#books' },
   { label: 'About', href: '#about' },
   { label: 'Contact', href: '#contact' }
